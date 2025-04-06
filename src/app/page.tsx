8ed5d1aad076d6fc804ca68117c82cc069ad2058
Thang Tran <kevindev0492@gmail.com>
2025-04-06 12:24:43 +0700

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,11 @@ export default function Home() {
                 <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
                 <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
                     <li className="mb-2 tracking-[-.01em]">
-                        Get started by editing{" "}
+                        Hello kute{" "}
                         <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-                            src/app/page.tsx
+                            E thang dep zai
                         </code>
-                        .
                     </li>
-                    <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
                 </ol>
 
                 <div className="flex gap-4 items-center flex-col sm:flex-row">
